1a7e33c25db870a6456abeb69782013f19fbb776
Võ Vĩ Khang <vovikhangcdv@gmail.com>
2021-07-20 09:58:53 +0700

    init project

---
+++ README.md
@@ -0,0 +1,25 @@
+# Multier
+
+The skeleton script used to customize to run multi-threaded scripts.
+
+## Usage
+
+Open file `multier.py` and: 
+- Step 1: Write your function
+
+- Step 2: Define input list of function
+
+- Step 3: Call multier function
+
+
+```python
+... # omit multier code
+def funName(data1, data2):
+    # TODO
+
+if __name__ == '__main__':
+    # TODO: dataList = ...
+    _input = [tuple(data[0], data[1]) for data in dataList]
+    NUM_THREAD = 10
+    multithreading(_input, funName, NUM_THREAD)
+```
---
+++ multier.py
@@ -0,0 +1,69 @@
+# Reference: https://sqlite.org/syntax/expr.html 
+
+from random import SystemRandom
+import requests, time, threading, queue, copy
+
+def multithreading(dataList, func, NUM_THREAD=10):
+    exitFlag = 0
+    start_time = time.time()
+
+    class MultiThread (threading.Thread):
+        def __init__(self, threadID, name, q, func):
+            threading.Thread.__init__(self)
+            self.threadID = threadID
+            self.name = name
+            self.q = q
+            self.func = copy.deepcopy(func)
+        def run(self):
+            print ("[+] Starting " + self.name)
+            process_data(self.name, self.q, self.func)
+            print ("[-] Exiting " + self.name)
+
+    
+    def process_data(threadName, q, func):
+        while not exitFlag:
+            if not workQueue.empty():
+                data = q.get()
+                func(*data)
+                print ("[+] %s processing %s" % (threadName, data))
+            else:
+                time.sleep(1)
+
+    threadList = ["Thread-" + str(i+1) for i in range(NUM_THREAD)]
+    # queueLock = threading.Lock()
+    workQueue = queue.Queue()
+    threads = []
+    threadID = 1
+
+    # Create new threads
+    for tName in threadList:
+        thread = MultiThread(threadID, tName, workQueue, func)
+        thread.start()
+        threads.append(thread)
+        threadID += 1
+
+    # Fill the queue
+    # queueLock.acquire()
+    for data in dataList:
+        workQueue.put(data)
+    # queueLock.release()
+
+    # Wait for queue to empty
+    while not workQueue.empty():
+        pass
+
+    # Notify threads it's time to exit
+    exitFlag = 1
+
+    # Wait for all threads to complete
+    for t in threads:
+        t.join()
+
+    total_time = (time.time() - start_time)
+    print("[*] Total time: %s seconds" % str(total_time))
+    print("[!] Exiting Main Thread")
+
+
+if __name__ == '__main__':
+    # TODO
+    pass
